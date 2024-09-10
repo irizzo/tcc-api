fc47e8e74f02ba457941306014282e27666defa4
Isabelle <isabelle965rizzo@gmail.com>
2024-09-10 17:50:58 -0300

    notes crud done

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,9 +3,9 @@ const verifyAccessToken = require('../middlewares/verifyAccessToken.js');
 module.exports = (app) => {
 	app.use('/categories', verifyAccessToken, require('./categories.routes'));
 	app.use('/events', verifyAccessToken, require('./events.routes'));
-	// app.use('/routines', verifyAccessToken, require('./routines.routes'));
+	app.use('/notes', verifyAccessToken, require('./notes.routes'))
 	app.use('/tasks', verifyAccessToken, require('./tasks.routes'));
 	app.use('/user', verifyAccessToken, require('./user.routes'));
-	app.use('/user-access', require('./userAccess.routes.js'));	
-	app.use('/test', require('./test.routes.js'));
+	app.use('/user-access', require('./userAccess.routes'));	
+	// app.use('/test', require('./test.routes'));
 }
