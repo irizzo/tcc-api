2bdc6367819d8278d01feda36e0761ec1d8e6a33
Isabelle <isabelle965rizzo@gmail.com>
2024-11-18 16:52:39 -0300

    fixing firebase config

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,7 +1,19 @@
 const { initializeApp, cert } = require('firebase-admin/app');
 const { getFirestore } = require('firebase-admin/firestore');
 
-const serviceAccount = require('./personal-organizer_firebaseCredentials.json');
+const serviceAccount = {
+	type: process.env.FBSERV_TYPE ,
+	project_id: process.env.FBSERV_PROJECT_ID ,
+	private_key_id: process.env.FBSERV_PRIVATE_KEY_ID ,
+	private_key: process.env.FBSERV_PRIVATE_KEY ,
+	client_email: process.env.FBSERV_CLIENT_EMAIL ,
+	client_id: process.env.FBSERV_CLIENT_ID ,
+	auth_uri: process.env.FBSERV_AUTH_URI,
+	token_uri: process.env.FBSERV_TOKEN_URI ,
+	auth_provider_x509_cert_url: process.env.FBSERV_AUTH_PROVIDER ,
+	client_x509_cert_url: process.env.FBSERV_CLIENT_CERT ,
+	universe_domain: process.env.FBSERV_UNIV_DOMAIN
+}
 
 initializeApp({
 	credential: cert(serviceAccount)
